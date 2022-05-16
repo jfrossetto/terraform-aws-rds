6d9a2c1d1c28f92ade19a694538b3ce9c5d9c539
Jeberson Fernando Rossetto <jeberson.fernandorossetto@thomsonreuters.com>
2022-05-16 17:43:05 -0300

    set multi_az

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ resource "aws_db_instance" "default" {
   allocated_storage    = 10
   engine               = "mysql"
   engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  name                 = "mydb"
-  username             = "foo"
-  password             = "foobarbaz"
+  instance_class       = var.instance_class
+  name                 = var.db_name
+  username             = var.db_user
+  password             = var.db_pass
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
-  multi_az = true
+  multi_az = var.multi_az
   apply_immediately = true
 }
