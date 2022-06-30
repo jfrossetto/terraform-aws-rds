cbc08026068ba91855b5022592ae9744a42d3bcf
Jeberson Fernando Rossetto <jeberson.fernandorossetto@thomsonreuters.com>
2022-06-30 17:44:40 -0300

    change parameter to db_name

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_db_instance" "default" {
   engine_version       = "5.7"
   identifier           = var.instance_name
   instance_class       = var.instance_class
-  name                 = var.db_name
+  db_name              = var.db_name
   username             = var.db_user
   password             = var.db_pass
   parameter_group_name = "default.mysql5.7"
